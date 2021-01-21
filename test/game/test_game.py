8124de55ffcd353e151f15f80be3aca0d7195084
chadfraser <chad.fraser@mail.mcgill.ca>
2021-01-21 08:05:28 -0500

    Minor changes to tests

--- test/game/test_game.py
+++ test/game/test_game.py
@@ -8,8 +8,8 @@ from locations.clearing import Clearing
 
 
 class TestGame(TestCase):
-    @patch('board_map.autumn_board_map.AutumnBoardMap')
-    def test_get_clearing(self, board_map_patch):
+    # @patch('board_map.autumn_board_map.AutumnBoardMap')
+    def test_get_clearing(self):
         game = Game()
         clearing1 = Clearing(game, Suit.FOX, priority=1, total_building_slots=1)
         clearing2 = Clearing(game, Suit.FOX, priority=2, total_building_slots=1)
@@ -17,63 +17,62 @@ class TestGame(TestCase):
 
         self.assertEqual(game.clearings(), [clearing1, clearing2])
 
-    @patch('board_map.autumn_board_map.AutumnBoardMap')
-    def test_get_clearings_of_suit(self, board_map_patch):
+    def test_get_clearings_of_suit(self):
         game = Game()
         clearing1 = Clearing(game, Suit.FOX, priority=1, total_building_slots=1)
         clearing2 = Clearing(game, Suit.FOX, priority=2, total_building_slots=1)
-        clearing3 = Clearing(game, Suit.FOX, priority=2, total_building_slots=1)
-        print(game)
-        print('/')
-        print(game.board_map)
-        game.board_map.get_clearings_of_suit.return_value = [clearing1, clearing2, clearing3]
+        clearing3 = Clearing(game, Suit.MOUSE, priority=2, total_building_slots=1)
+        game.board_map.get_clearings_of_suit.return_value = lambda c: [clearing1, clearing2, clearing3]
 
         self.assertEqual(game.get_clearings_of_suit(Suit.FOX), [clearing1, clearing2])
 
-    # def test_get_corner_clearings(self):
-    #     mock_game = Mock()
+    def test_get_clearings_of_suit_bird(self):
+        game = Game()
+        clearing1 = Clearing(game, Suit.FOX, priority=1, total_building_slots=1)
+        clearing2 = Clearing(game, Suit.FOX, priority=2, total_building_slots=1)
+        clearing3 = Clearing(game, Suit.MOUSE, priority=2, total_building_slots=1)
+        game.board_map.get_clearings_of_suit.return_value = lambda c: [clearing1, clearing2, clearing3]
+
+        self.assertEqual(game.get_clearings_of_suit(Suit.BIRD), [clearing1, clearing2, clearing3])
+
+    # TODO: All below
+    # discard card
+    # send card to discard pile (with and without Cultesque player; with or without Dom card)
+    # craft item (is or isn't available; with or without score)
+    # win
+    #
+    # def test_initialize_item_supply(self):
+    #     game = Game()
     #     clearing1 = Clearing(mock_game, Suit.FOX, priority=1, total_building_slots=1, is_corner_clearing=True)
     #     clearing2 = Clearing(mock_game, Suit.FOX, priority=2, total_building_slots=1, is_corner_clearing=False)
     #     board_map = BoardMap(mock_game)
     #     board_map.clearings = [clearing1, clearing2]
     #
     #     self.assertEqual(board_map.get_corner_clearings(), [clearing1])
-
-    # TODO
-    # @patch('board_map.board_map.BoardMap.__abstractmethods__', set())# game.get_number_of_items_p_ruin -> 1
-    # def test_initialize_ruins(self):
-    #     mock_game = Mock()
-    #     board_map = BoardMap(mock_game)
-    #     # Assert that the result is four item tokens, BOOT, BAG, HAMMER, SWORD, in any order
-    #     board_map.initialize_ruins()
     #
-    # @patch('board_map.board_map.BoardMap.__abstractmethods__', set())# game.get_number_of_items_per_ruin ->
-    # def test_initialize_ruins_multiple_items_per_ruin(self):
-    #     mock_game = Mock()
+    # def test_get_number_of_items_per_ruin(self):
+    #     game = Game()
+    #     clearing1 = Clearing(mock_game, Suit.FOX, priority=1, total_building_slots=1, is_corner_clearing=True)
+    #     clearing2 = Clearing(mock_game, Suit.FOX, priority=2, total_building_slots=1, is_corner_clearing=False)
     #     board_map = BoardMap(mock_game)
-    #     # Assert that the result is eight item tokens, two of each BOOT, BAG, HAMMER, SWORD, in any order
-    #     board_map.initialize_ruins()
-
-    # @patch('board_map.board_map.BoardMap.__abstractmethods__', set())
-    # def test_get_clearings_of_suit(self):
-    #     mock_game = Mock()
-    #     clearing1 = Clearing(mock_game, Suit.FOX, priority=1, total_building_slots=1)
-    #     clearing2 = Clearing(mock_game, Suit.RABBIT, priority=2, total_building_slots=1)
-    #     clearing3 = Clearing(mock_game, Suit.FOX, priority=3, total_building_slots=1)
-    #     clearing4 = Clearing(mock_game, Suit.MOUSE, priority=4, total_building_slots=1)
+    #     board_map.clearings = [clearing1, clearing2]
+    #
+    #     self.assertEqual(board_map.get_corner_clearings(), [clearing1])
+    #
+    # def test_get_item_if_available__true(self):
+    #     game = Game()
+    #     clearing1 = Clearing(mock_game, Suit.FOX, priority=1, total_building_slots=1, is_corner_clearing=True)
+    #     clearing2 = Clearing(mock_game, Suit.FOX, priority=2, total_building_slots=1, is_corner_clearing=False)
     #     board_map = BoardMap(mock_game)
-    #     board_map.clearings = [clearing1, clearing2, clearing3, clearing4]
+    #     board_map.clearings = [clearing1, clearing2]
     #
-    #     self.assertEqual(board_map.get_clearings_of_suit(Suit.FOX), [clearing1, clearing3])
+    #     self.assertEqual(board_map.get_corner_clearings(), [clearing1])
     #
-    # @patch('board_map.board_map.BoardMap.__abstractmethods__', set())
-    # def test_get_clearings_of_suit_bird(self):
-    #     mock_game = Mock()
-    #     clearing1 = Clearing(mock_game, Suit.FOX, priority=1, total_building_slots=1)
-    #     clearing2 = Clearing(mock_game, Suit.RABBIT, priority=2, total_building_slots=1)
-    #     clearing3 = Clearing(mock_game, Suit.FOX, priority=3, total_building_slots=1)
-    #     clearing4 = Clearing(mock_game, Suit.MOUSE, priority=4, total_building_slots=1)
+    # def test_get_item_if_available__false(self):
+    #     game = Game()
+    #     clearing1 = Clearing(mock_game, Suit.FOX, priority=1, total_building_slots=1, is_corner_clearing=True)
+    #     clearing2 = Clearing(mock_game, Suit.FOX, priority=2, total_building_slots=1, is_corner_clearing=False)
     #     board_map = BoardMap(mock_game)
-    #     board_map.clearings = [clearing1, clearing2, clearing3, clearing4]
+    #     board_map.clearings = [clearing1, clearing2]
     #
-    #     self.assertEqual(board_map.get_clearings_of_suit(Suit.BIRD), [clearing1, clearing2, clearing3, clearing4])
+    #     self.assertEqual(board_map.get_corner_clearings(), [clearing1])
